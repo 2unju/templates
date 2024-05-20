1f5e6b843ae1f7605a704d16fdd60d151e6b6871
2unju <qdw0313@khu.ac.kr>
2024-05-21 08:41:04 +0900

    Add: spinner with modal

---
+++ spinner_with_modal.py
@@ -0,0 +1,23 @@
+import time
+import streamlit as st
+from streamlit_modal import Modal
+
+# With Modal
+# site-package의 streamlit-modal > __init__.py도 함께 수정
+modal = Modal(
+    title="Please wait",
+    key="testModal",
+    max_width=500
+)
+
+if modal.is_open():
+    with modal.container():
+        with st.spinner(text="잠시만 기다려주세요..."):
+            st.markdown("<p style='font-size: 14px; color: Gray;'>이 작업은 몇 분 정도 걸릴 수 있습니다.</p>",
+                        unsafe_allow_html=True)
+            time.sleep(5)
+            modal.close()
+success = False
+
+if st.button("spinner", key="spin", disabled=st.session_state.wait_button_disable):
+    modal.open()
